e143d9a300296c677a57a9a4ea2a34c9701b238f
moofoo <nathancookdev@gmail.com>
2019-10-17 18:23:36 -0500

    removing notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { useMutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { withRouter } from 'react-router';
 import {
   Button,
   Container,
@@ -6,6 +9,28 @@ import {
   TextField
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+
+const QUERY_NOTES = gql`
+  {
+    notes {
+      id
+      text
+      title
+      date
+    }
+  }
+`;
+
+const REMOVE_NOTE = gql`
+  mutation RemoveNote($id: Int!) {
+    removeNote(id: $id) {
+      id
+      title
+      text
+      date
+    }
+  }
+`;
 
 const useStyles = makeStyles(() => ({
   container: {
@@ -18,15 +43,32 @@ const useStyles = makeStyles(() => ({
     width: 500
   },
   button: {
-    marginTop: 15
+    marginTop: 15,
+    marginRight: 15
   }
 }));
 
-const Note = ({ note, action, onSubmit }) => {
+const Note = ({ note, action, onSubmit, history }) => {
   const classes = useStyles();
 
   const [title, setTitle] = useState(note ? note.title : '');
   const [text, setText] = useState(note ? note.text : '');
+
+  const [removeNote] = useMutation(REMOVE_NOTE, {
+    update(cache) {
+      const { notes } = cache.readQuery({ query: QUERY_NOTES });
+
+      cache.writeQuery({
+        query: QUERY_NOTES,
+        data: { notes: notes.filter(nt => nt.id !== note.id) }
+      });
+    }
+  });
+
+  const onRemoveHandler = () => {
+    removeNote({ variables: { id: note.id } });
+    history.push('/');
+  };
 
   return (
     <Container className={classes.container}>
@@ -59,9 +101,19 @@ const Note = ({ note, action, onSubmit }) => {
         >
           {action === 'edit' ? 'Update' : 'Add'}
         </Button>
+        {action === 'edit' ? (
+          <Button
+            className={classes.button}
+            variant='contained'
+            component='div'
+            onClick={onRemoveHandler}
+          >
+            Remove
+          </Button>
+        ) : null}
       </div>
     </Container>
   );
 };
 
-export default Note;
+export default withRouter(Note);
